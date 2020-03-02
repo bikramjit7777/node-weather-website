1b08c07de4659a07aaed2f966b68a970132b7879
Bikramjit Singh <bikramjit7777@gmail.com>
2020-03-02 06:30:31 +0530

    Adding max and min temperature

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -2,6 +2,8 @@ const request = require('request')
 const forecast = (latitude,longitude,callback) =>{
     const url ='https://api.darksky.net/forecast/3e40cca877141d06e81739ea49bbd05e/'+latitude+','+longitude
     request({url, json: true},(error,{body})=>{
+        console.log('lat: '+latitude)
+        console.log('long: '+longitude)
         if(error){
                     callback('Unable to connect to weather service',undefined)
                 }
@@ -9,7 +11,7 @@ const forecast = (latitude,longitude,callback) =>{
                     callback('unable to find location.',undefined)
                 }
                 else {
-                    callback(undefined,'it is currently '+body.currently.temperature+' degrees out and there is '+body.currently.precipProbability+'% chances of rain.')
+                    callback(undefined,body.daily.data[0].summary + ' it is currently '+body.currently.temperature+' degrees out and there is '+body.currently.precipProbability+'% chances of rain.'+' Highest temperature was '+ body.daily.data[0].temperatureHigh +' and lowest temperature was '+ body.daily.data[0].temperatureLow+'.')
               
                 }
     }
